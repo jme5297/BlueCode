2ed99cb5b376d1b0f1361407c95fa698444f862b
Jason <jme5297@psu.edu>
2018-02-27 20:22:36 -0500

    Camera fix

--- src/Camera_generic.cpp
+++ src/Camera_generic.cpp
@@ -1,25 +1,25 @@
 #include <sensors/Camera/Camera_generic.h>
-#define CLEAR(x) memset(&(x), 0, sizeof(x))
 using namespace sensors;
 
+#ifdef USE_CAMERA
+
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
 struct buffer {
     void   *start;
     size_t length;
 };
-
 static void xioctl(int fh, int request, void *arg)
 {
     int r;
-    
     do {
         r = v4l2_ioctl(fh, request, arg);
     } while (r == -1 && ((errno == EINTR) || (errno == EAGAIN)));
-    
     if (r == -1) {
         fprintf(stderr, "error %d, %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
+#endif
 
 Camera::Camera(){
 
@@ -68,13 +68,13 @@ bool Camera::TakeImage(int a){
     FILE                            *fout;
     struct buffer                   *buffers;
     int                             p;
-    
+
     fd = v4l2_open(dev_name, O_RDWR | O_NONBLOCK, 0);
     if (fd < 0) {
         perror("Cannot open device");
         exit(EXIT_FAILURE);
     }
-    
+
     CLEAR(fmt);
     fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     fmt.fmt.pix.width       = 1920;
@@ -89,35 +89,35 @@ bool Camera::TakeImage(int a){
     if ((fmt.fmt.pix.width != 1920) || (fmt.fmt.pix.height != 1080))
         printf("Warning: driver is sending image at %dx%d\n",
                fmt.fmt.pix.width, fmt.fmt.pix.height);
-    
+
     CLEAR(req);
     req.count = 2;
     req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     req.memory = V4L2_MEMORY_MMAP;
     xioctl(fd, VIDIOC_REQBUFS, &req);
-    
+
     buffers = (buffer*)calloc(req.count, sizeof(*buffers));
 
     for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
         CLEAR(buf);
-        
+
         buf.type        = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         buf.memory      = V4L2_MEMORY_MMAP;
         buf.index       = n_buffers;
-        
+
         xioctl(fd, VIDIOC_QUERYBUF, &buf);
-        
+
         buffers[n_buffers].length = buf.length;
         buffers[n_buffers].start = v4l2_mmap(NULL, buf.length,
                                              PROT_READ | PROT_WRITE, MAP_SHARED,
                                              fd, buf.m.offset);
-        
+
         if (MAP_FAILED == buffers[n_buffers].start) {
             perror("mmap");
             exit(EXIT_FAILURE);
         }
     }
-    
+
     for (i = 0; i < n_buffers; ++i) {
         CLEAR(buf);
         buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
@@ -126,7 +126,7 @@ bool Camera::TakeImage(int a){
         xioctl(fd, VIDIOC_QBUF, &buf);
     }
     type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    
+
     xioctl(fd, VIDIOC_STREAMON, &type);
     /*for (i = 0; i < 5; i++) {*/
     /*printf("Enter the location point\n");
@@ -134,23 +134,23 @@ bool Camera::TakeImage(int a){
     do {
         FD_ZERO(&fds);
         FD_SET(fd, &fds);
-        
+
         /* Timeout. */
         tv.tv_sec = 2;
         tv.tv_usec = 0;
-        
+
         r = select(fd + 1, &fds, NULL, NULL, &tv);
     } while ((r == -1 && (errno = EINTR)));
     if (r == -1) {
         perror("select");
         return errno;
     }
-    
+
     CLEAR(buf);
     buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     buf.memory = V4L2_MEMORY_MMAP;
     xioctl(fd, VIDIOC_DQBUF, &buf);
-    
+
     sprintf(out_name, "image%d.ppm", a);
     fout = fopen(out_name, "w");
     if (!fout) {
@@ -161,10 +161,10 @@ bool Camera::TakeImage(int a){
             fmt.fmt.pix.width, fmt.fmt.pix.height);
     fwrite(buffers[buf.index].start, buf.bytesused, 1, fout);
     fclose(fout);
-    
+
     xioctl(fd, VIDIOC_QBUF, &buf);
     /*}*/
-    
+
     type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     xioctl(fd, VIDIOC_STREAMOFF, &type);
     for (i = 0; i < n_buffers; ++i)
