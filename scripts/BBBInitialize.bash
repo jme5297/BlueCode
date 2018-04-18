50906e3bae406e76b044076bbe4d44cf7892bce0
root <root@beaglebone.localdomain>
2018-04-18 21:18:28 +0000

    Added scripts

---
+++ scripts/BBBInitialize.bash
@@ -0,0 +1,8 @@
+echo Hello!
+#echo Building interface code...
+#cd InterfaceCode
+#sudo bash buildAll.bash
+echo Setting up BBB for BlueCode...
+cd ../BlueCode
+bash BBBsetup.bash
+echo Setup complete! Go to BlueCode/build to continue mission setup.
